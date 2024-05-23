8b40bf010f360825747cc2ce4576705e86d4b1ce
Erick Zanardo <erickzanardoo@gmail.com>
2024-05-23 15:28:53 -0300

    PR comments

--- packages/shorebird_tests/test/ios_test.dart
+++ packages/shorebird_tests/test/ios_test.dart
@@ -3,47 +3,48 @@ import 'package:test/test.dart';
 import 'shorebird_tests.dart';
 
 void main() {
-  group('shorebird ios projects', () {
-    testWithShorebirdProject('can build', (projectDirectory) async {
-      await projectDirectory.runFlutterBuildIos();
+  group(
+    'shorebird ios projects',
+    () {
+      testWithShorebirdProject('can build', (projectDirectory) async {
+        await projectDirectory.runFlutterBuildIos();
 
-      expect(projectDirectory.iosArchiveFile().existsSync(), isTrue);
-      expect(projectDirectory.getGeneratedIoShorebirdYaml(), completes);
-    });
+        expect(projectDirectory.iosArchiveFile().existsSync(), isTrue);
+        expect(projectDirectory.getGeneratedIoShorebirdYaml(), completes);
+      });
 
-    group('when passing the public key through the environment variable', () {
-      testWithShorebirdProject(
-        'adds the public key on top of the original file',
-        (projectDirectory) async {
-          final originalYaml = projectDirectory.shorebirdYaml;
+      group('when passing the public key through the environment variable', () {
+        testWithShorebirdProject(
+          'adds the public key on top of the original file',
+          (projectDirectory) async {
+            final originalYaml = projectDirectory.shorebirdYaml;
 
-          const base64PublicKey = 'public_123';
-          await projectDirectory.runFlutterBuildIos(
-            environment: {
-              'SHOREBIRD_PUBLIC_KEY': base64PublicKey,
-            },
-          );
+            const base64PublicKey = 'public_123';
+            await projectDirectory.runFlutterBuildIos(
+              environment: {
+                'SHOREBIRD_PUBLIC_KEY': base64PublicKey,
+              },
+            );
 
-          final generatedYaml =
-              await projectDirectory.getGeneratedIoShorebirdYaml();
+            final generatedYaml =
+                await projectDirectory.getGeneratedIoShorebirdYaml();
 
-          expect(
-            generatedYaml.keys,
-            containsAll(originalYaml.keys),
-          );
+            expect(
+              generatedYaml.keys,
+              containsAll(originalYaml.keys),
+            );
 
-          print(generatedYaml);
-          expect(
-            generatedYaml['patch_public_key'],
-            equals(base64PublicKey),
-          );
-        },
-      );
-    });
+            print(generatedYaml);
+            expect(
+              generatedYaml['patch_public_key'],
+              equals(base64PublicKey),
+            );
+          },
+        );
+      });
 
-    // TODO(erickzanardo): Add tests for flavors.
-  }, onPlatform: {
-    'windows': const Skip('iOS tests are not supported on Windows'),
-    'linux': const Skip('iOS tests are not supported on Linux'),
-  });
+      // TODO(erickzanardo): Add tests for flavors.
+    },
+    testOn: 'mac-os',
+  );
 }
