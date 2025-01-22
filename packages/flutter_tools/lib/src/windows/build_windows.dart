3305c6de99ed63a93ae1b3673cdcfc651e047b57
bryanoltman <bryan@shorebird.dev>
2025-01-21 22:59:19 -0500

    remove comments

--- packages/flutter_tools/lib/src/windows/build_windows.dart
+++ packages/flutter_tools/lib/src/windows/build_windows.dart
@@ -135,15 +135,6 @@ Future<void> buildWindows(
     .childDirectory('flutter_assets')
     .childFile('shorebird.yaml');
 
-  // if (shorebirdYamlFile.existsSync()) {
-  //   try {
-  //     updateShorebirdYaml(buildInfo.flavor, shorebirdYamlFile.path, environment: globals.platform.environment);
-  //   } on Exception catch (error) {
-  //     globals.printError('[shorebird] failed to generate shorebird configuration.\n$error');
-  //     throw Exception('Failed to generate shorebird configuration');
-  //   }
-  // }
-
   if (buildInfo.codeSizeDirectory != null && sizeAnalyzer != null) {
     final String arch = getNameForTargetPlatform(targetPlatform);
     final File codeSizeFile = globals.fs.directory(buildInfo.codeSizeDirectory)
