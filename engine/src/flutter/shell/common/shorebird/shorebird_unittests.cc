2b9e09229d0534b14be35af71ba7312267391527
Bryan Oltman <bryan@shorebird.dev>
2025-06-16 17:54:00 -0400

    fix tests

--- engine/src/flutter/shell/common/shorebird/shorebird_unittests.cc
+++ engine/src/flutter/shell/common/shorebird/shorebird_unittests.cc
@@ -2,16 +2,20 @@
 
 #include "gtest/gtest.h"
 
+namespace flutter {
+namespace testing {
 TEST(Shorebird, GetValueFromYamlValueExists) {
   std::string yaml = "appid: com.example.app\nversion: 1.0.0\n";
   std::string key = "appid";
-  std::string value = get_value_from_yaml(yaml, key);
+  std::string value = GetValueFromYaml(yaml, key);
   EXPECT_EQ(value, "com.example.app");
 }
 
 TEST(Shorebird, GetValueFromYamlValueDoesNotExist) {
   std::string yaml = "appid: com.example.app\nversion: 1.0.0\n";
   std::string key = "appid2";
-  std::string value = get_value_from_yaml(yaml, key);
+  std::string value = GetValueFromYaml(yaml, key);
   EXPECT_EQ(value, "");
 }
+}  // namespace testing
+}  // namespace flutter
