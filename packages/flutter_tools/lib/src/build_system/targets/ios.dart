c05e11da9573bd5f6ae523fc44ddd6af75c8351b
Eric Seidel <eric@shorebird.dev>
2025-11-14 14:22:52 -0800

    chore: squash all commits on top of 3.35.7

--- packages/flutter_tools/lib/src/build_system/targets/ios.dart
+++ packages/flutter_tools/lib/src/build_system/targets/ios.dart
@@ -143,6 +143,12 @@ abstract class AotAssemblyBase extends Target {
       // Don't fail if the dSYM wasn't created (i.e. during a debug build).
       skipMissingInputs: true,
     );
+
+    await LinkSupplement.create(
+      environment,
+      inputBuildDir: buildOutputPath,
+      outputBuildDir: getIosBuildDirectory(),
+    );
   }
 }
 
