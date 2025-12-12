25bd5509cc64be1454e2a8d87e3870473a80fd2a
Eric Seidel <eric@shorebird.dev>
2025-12-12 14:36:18 -0800

    feat: shorebird flutter should work without setting FLUTTER_STORAGE_BASE_URL (#97)
    * fix: make dart/flutter work without FLUTTER_STORAGE_BASE_URL
    
    * feat: shorebird flutter should work without setting FLUTTER_STORAGE_BASE_URL
    
    * fix: flutter_tools test fixes
    
    * fix: enable running flutter_tools tests
    
    * chore: remove unnecessary workflow

--- packages/flutter_tools/lib/src/cache.dart
+++ packages/flutter_tools/lib/src/cache.dart
@@ -532,7 +532,7 @@ class Cache {
   /// The base for URLs that store Flutter engine artifacts that are fetched
   /// during the installation of the Flutter SDK.
   ///
-  /// By default the base URL is https://storage.googleapis.com. However, if
+  /// By default the base URL is https://download.shorebird.dev. However, if
   /// `FLUTTER_STORAGE_BASE_URL` environment variable ([kFlutterStorageBaseUrl])
   /// is provided, the environment variable value is returned instead.
   ///
@@ -544,7 +544,7 @@ class Cache {
     String? overrideUrl = _platform.environment[kFlutterStorageBaseUrl];
     if (overrideUrl == null) {
       return storageRealm.isEmpty
-          ? 'https://storage.googleapis.com'
+          ? 'https://download.shorebird.dev'
           : 'https://storage.googleapis.com/$storageRealm';
     }
     // Shorebird's artifact proxy is a trusted source.
