3f02baf5696e3223d4aae0815f6effb5f40bd6c6
Eric Seidel <eric@shorebird.dev>
2025-12-12 09:04:44 -0800

    feat: make it possible to load two patches into the runtime at once (#96)
    * fix: stop using globals for patch data
    
    * chore: run et format
    
    * chore: add missing files
    
    * test: add unittest
    
    * chore: run et format
    
    * chore: move elf_cache down into runtime
    
    * chore: rename elf* to patch*
    
    * chore: clean up logs
    
    * chore: clean up comments
    
    * chore: use Shorebird dart
    
    * chore: small cleanup

--- bin/internal/update_dart_sdk.sh
+++ bin/internal/update_dart_sdk.sh
@@ -123,7 +123,7 @@ if [ ! -f "$ENGINE_STAMP" ] || [ "$ENGINE_VERSION" != `cat "$ENGINE_STAMP"` ]; t
     FIND=find
   fi
 
-  DART_SDK_BASE_URL="${FLUTTER_STORAGE_BASE_URL:-https://storage.googleapis.com}${ENGINE_REALM:+/$ENGINE_REALM}"
+  DART_SDK_BASE_URL="${FLUTTER_STORAGE_BASE_URL:-https://download.shorebird.dev}${ENGINE_REALM:+/$ENGINE_REALM}"
   DART_SDK_URL="$DART_SDK_BASE_URL/flutter_infra_release/flutter/$ENGINE_VERSION/$DART_ZIP_NAME"
 
   # if the sdk path exists, copy it to a temporary location
