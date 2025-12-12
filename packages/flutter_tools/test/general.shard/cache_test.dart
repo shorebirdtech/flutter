a7f1ec66c0c120b50f19b977268796c1fa8e5c11
Eric Seidel <eric@shorebird.dev>
2025-12-12 14:36:18 -0800

    feat: shorebird flutter should work without setting FLUTTER_STORAGE_BASE_URL (#97)
    * fix: make dart/flutter work without FLUTTER_STORAGE_BASE_URL
    
    * feat: shorebird flutter should work without setting FLUTTER_STORAGE_BASE_URL
    
    * fix: flutter_tools test fixes
    
    * fix: enable running flutter_tools tests
    
    * chore: remove unnecessary workflow

--- packages/flutter_tools/test/general.shard/cache_test.dart
+++ packages/flutter_tools/test/general.shard/cache_test.dart
@@ -1406,7 +1406,7 @@ void main() {
       expect(messages, <String>['Web SDK']);
 
       expect(downloads, <String>[
-        'https://storage.googleapis.com/flutter_infra_release/flutter/hijklmnop/flutter-web-sdk.zip',
+        'https://download.shorebird.dev/flutter_infra_release/flutter/hijklmnop/flutter-web-sdk.zip',
       ]);
 
       expect(locations, <String>['/bin/cache/flutter_web_sdk']);
@@ -1531,7 +1531,7 @@ void main() {
     expect(messages, <String>['Engine Information']);
 
     expect(downloads, <String>[
-      'https://storage.googleapis.com/flutter_infra_release/flutter/hijklmnop/engine_stamp.json',
+      'https://download.shorebird.dev/flutter_infra_release/flutter/hijklmnop/engine_stamp.json',
     ]);
     expect(locations, <String>['/bin/cache']);
     // file copy is done by the real uploader; not the fake.
