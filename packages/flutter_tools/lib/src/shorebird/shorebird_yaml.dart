d295a25eb5480ecad4ddd68d0728a3444cd98ed0
Eric Seidel <eric@shorebird.dev>
2026-01-11 14:34:50 -0800

    feat: allow patch_verification_mode

--- packages/flutter_tools/lib/src/shorebird/shorebird_yaml.dart
+++ packages/flutter_tools/lib/src/shorebird/shorebird_yaml.dart
@@ -56,6 +56,7 @@ Map<String, dynamic> compileShorebirdYaml(YamlMap yamlMap, {required String? fla
   }
   copyIfSet('base_url');
   copyIfSet('auto_update');
+  copyIfSet('patch_verification_mode');
   final String? shorebirdPublicKeyEnvVar = environment['SHOREBIRD_PUBLIC_KEY'];
   if (shorebirdPublicKeyEnvVar != null) {
     compiled['patch_public_key'] = shorebirdPublicKeyEnvVar;
